84d7f5cd7d3c896db1950c4b9948fe6ce292f1a2
Muru_C <mc6251@nyu.edu>
2019-04-16 03:35:40 -0400

    blood particle, pop up text, fix some bugs

---
+++ Assets/Scripts/PopText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopText : MonoBehaviour
+{
+    public float aliveTime;
+    public float offsetX;
+    public float offsetY;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        transform.position = new Vector2(transform.position.x + offsetX, transform.position.y + offsetY);
+
+        Destroy(gameObject, aliveTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
